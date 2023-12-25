61902840e491ff49358b5463048b363c18e211be
Sebastian Massella <sebastianmassella@yahoo.com>
2023-12-25 14:20:41 -0500

    First Commit

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+config()
+
+import Deck from './models/deck';
+
+
+const PORT = 8000;
+const HOST = 'localhost'
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/decks', async (req: Request, res: Response) => {
+    const newDeck = new Deck({
+        title: req.body.title 
+    });
+    const createdDeck = await newDeck.save();
+    res.setHeader('Content-Type', 'application/json')
+    res.status(201);
+    res.json(createdDeck);
+});
+
+
+mongoose.connect(process.env.MONGO_URL!)
+.then( () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`)
+    app.listen(PORT);
+});
+
+
