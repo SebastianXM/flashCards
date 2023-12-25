068d27d0a796616eb50b4494fa0f4924d0e837bb
Sebastian Massella <sebastianmassella@yahoo.com>
2023-12-25 16:57:08 -0500

    css styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Deck from './Deck.tsx';
 
 const router = createBrowserRouter([
